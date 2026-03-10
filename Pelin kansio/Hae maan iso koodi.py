f852debc5b5185e313dd4e517e3a880f83f56fde
lauripussinen <lauri.pussinen@gmail.com>
2026-03-10 20:56:48 +0200

    Pääpelin muokkauksia

--- Pelin kansio/Hae maan iso koodi.py
+++
@@ -1,8 +0,0 @@
-def hae_maan_iso_koodi(nimi):
-    sql = 'SELECT iso_country FROM country WHERE name = %s'
-    cursor = yhteys.cursor(dictionary=True)
-    cursor.execute(sql, (nimi,))
-    tulos = cursor.fetchone()
-    cursor.close()
-    if tulos:
-        return tulos["iso_country"]
